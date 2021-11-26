95ebbb05b00ecaafbe8320b5b7fa5e759fcc2782
kanghanhee <kanghanhee59@gmail.com>
2021-11-26 14:44:41 +0900

    feat: 파이어베이스 초기 세팅, 배너조회 및 메시지 보내기 api 구현

---
+++ functions/constants/responseMessage.js
@@ -0,0 +1,11 @@
+module.exports = {
+  NULL_VALUE: "필요한 값이 없습니다",
+  OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+
+  // 메인화면_배너
+  SUCCESS_CREATED_MESSAGE: "메시지 보내기 성공",
+  SUCCESS_GET_BANNER: "배너 조회 성공",
+
+  // 서버 내 오류
+  INTERNAL_SERVER_ERROR: "서버 내 오류",
+};
